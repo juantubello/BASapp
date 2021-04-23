aa7b807065a4bb9468a1e14e8dabf3daa9c35196
Juan Pablo Fernandez Tubello <juanfernatubello@gmail.com>
2021-04-23 18:25:05 +0000

    Refactoring to use models

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,15 +1,24 @@
-sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-],
-	/**
-	 * @param {typeof sap.ui.core.mvc.Controller} Controller
-	 */
-	function (Controller) {
-		"use strict";
+sap.ui.define(
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel",
+    "com/firstbassapp/firstapp/model/models",
+  ],
+  /**
+   * @param {typeof sap.ui.core.mvc.Controller} Controller
+   */
+  function (Controller, MessageToast, JSONModel, models) {
+    "use strict";
 
-		return Controller.extend("com.firstbassapp.firstapp.controller.View1", {
-			onInit: function () {
-
-			}
-		});
-	});
+    return Controller.extend("com.firstbassapp.firstapp.controller.View1", {
+      onInit: function () {
+        //set model on view
+        this.getView().setModel(models.createRecipient());
+      },
+      onShowHello: function () {
+        MessageToast.show("Hello!");
+      },
+    });
+  }
+);
