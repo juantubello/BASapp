26212d1bf526adcddeb3d7b8ee4178f0db12b610
Juan Pablo Fernandez Tubello <juanfernatubello@gmail.com>
2021-04-23 22:04:20 +0000

    Uso de fragments y dialogs

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -1,12 +1,9 @@
 sap.ui.define(
-  [
-    "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
-  ],
+  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast", "sap/ui/core/Fragment"],
   /**
    * @param {typeof sap.ui.core.mvc.Controller} Controller
    */
-  function (Controller, MessageToast) {
+  function (Controller, MessageToast, Fragment) {
     "use strict";
 
     return Controller.extend(
@@ -15,6 +12,21 @@ sap.ui.define(
         onInit: function () {},
         onShowHello: function () {
           MessageToast.show("Hello!");
+        },
+        onOpenDialog: function () {
+          let oView = this.getView();
+
+          if (!this.byId("helloDialog")) {
+            Fragment.load({
+              id: oView.getId(),
+              name: "com.firstbassapp.firstapp.view.HelloDialog",
+            }).then(function (oDialog) {
+              oView.addDependent(oDialog);
+              oDialog.open();
+            });
+          }else{
+              this.byId("helloDialog").open();
+          }
         },
       }
     );
