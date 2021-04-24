a2729337a535cd819cef1000704838cd38096a03
Juan Pablo Fernandez Tubello <juanfernatubello@gmail.com>
2021-04-24 16:21:47 +0000

    Uso de custom formatter

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -1,15 +1,16 @@
 sap.ui.define(
-  ["sap/ui/core/mvc/Controller", "sap/ui/model/json/JSONModel"],
+  ["sap/ui/core/mvc/Controller", "sap/ui/model/json/JSONModel", "../model/formatter"],
   /**
    * @param {typeof sap.ui.core.mvc.Controller} Controller
    */
-  function (Controller, JSONModel) {
+  function (Controller, JSONModel, formatter) {
     "use strict";
 
     return Controller.extend(
       "com.firstbassapp.firstapp.controller.InvoiceList",
       {
-
+        formatter: formatter,
+        
         onInit: function () {
             let oViewModel = new JSONModel({
                 currency: "EUR"
---
+++ webapp/model/formatter.js
@@ -0,0 +1,18 @@
+sap.ui.define([], function () {
+  return {
+    statusText: function (sStatus) {
+      let resourceBundle = this.getView().getModel("i18n").getResourceBundle();
+
+      switch (sStatus) {
+        case "A":
+          return resourceBundle.getText("invoiceStatusA");
+        case "B":
+          return resourceBundle.getText("invoiceStatusB");
+        case "C":
+          return resourceBundle.getText("invoiceStatusC");
+        default:
+          return sStatus;
+      }
+    }
+  };
+});
