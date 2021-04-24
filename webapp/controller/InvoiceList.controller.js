d84f1430cfac9ce4ea6cd7768061f6ea24a6775f
Juan Pablo Fernandez Tubello <juanfernatubello@gmail.com>
2021-04-24 15:17:25 +0000

    Uso de Binding agregation y Formateadores

---
+++ webapp/controller/InvoiceList.controller.js
@@ -0,0 +1,22 @@
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/ui/model/json/JSONModel"],
+  /**
+   * @param {typeof sap.ui.core.mvc.Controller} Controller
+   */
+  function (Controller, JSONModel) {
+    "use strict";
+
+    return Controller.extend(
+      "com.firstbassapp.firstapp.controller.InvoiceList",
+      {
+
+        onInit: function () {
+            let oViewModel = new JSONModel({
+                currency: "EUR"
+            });
+            this.getView().setModel(oViewModel, "view");
+        },
+      }
+    );
+  }
+);
