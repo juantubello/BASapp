e514806035354a0aab1ef874d8a776f0e98e2f70
Juan Pablo Fernandez Tubello <juanfernatubello@gmail.com>
2021-04-28 20:22:59 +0000

    Controles personalizados XD

--- webapp/controller/Details.controller.js
+++ webapp/controller/Details.controller.js
@@ -1,22 +1,23 @@
 sap.ui.define(
   [
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel",
     "sap/ui/core/routing/History",
-    "sap/ui/core/UIComponent",
+    "sap/ui/core/UIComponent"
   ],
   /**
    * @param {typeof sap.ui.core.mvc.Controller} Controller
    * @param {typeof sap.ui.core.routing.History} History
    * @param {typeof sap.ui.core.UIComponent} UIComponent
    */
-  function (Controller, MessageToast, JSONModel, History, UIComponent) {
+  function (Controller, History, UIComponent) {
     "use strict";
 
     return Controller.extend("com.firstbassapp.firstapp.controller.Details", {
 
         _onObjectMatch: function(oEvent){
+
+            this.byId("rating").reset();
+
             this.getView().bindElement({
                 path: "/" + window.decodeURIComponent(oEvent.getParameter("arguments").invoicePath),
                 model: "invoice"
@@ -26,6 +27,7 @@ sap.ui.define(
       onInit: function () {
             const oRouter = sap.ui.core.UIComponent.getRouterFor(this);
             oRouter.getRoute("Details").attachPatternMatched(this._onObjectMatch, this);
+
       },
       
       onNavBack: function(){
@@ -39,6 +41,12 @@ sap.ui.define(
             const oRouter = UIComponent.getRouterFor(this);
             oRouter.navTo("RouteView1", {}, true);
         }
+      },
+
+      onRatingChange: function(oEvent){
+          const fValue = oEvent.getParameter("value");
+          const oResourceBundle = this.getView().getModel("i18n").getResourceBundle();
+          sap.m.MessageToast.show(oResourceBundle.getText("ratingConfirmation", [fValue]));
       }
 
     });
